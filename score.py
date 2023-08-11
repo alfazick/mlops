43b35dd0e2c5ebec77d8db8f9585c717b4822177
Askar Nurbekov <nurbekoff@gmail.com>
2023-08-11 02:18:09 -0500

    Update score.py
    Works on local vscode

--- score.py
+++ score.py
@@ -1,31 +1,61 @@
 import json
 import numpy as np
-from tensorflow.keras.models import load_model
-from azureml.core.model import Model
-from PIL import Image
 import requests
 from io import BytesIO
-
+from PIL import Image
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from azureml.core import Workspace
+from azureml.core.model import Model as AzureModel
+import os
 def init():
-    global model
-    #model_path = Model.get_model_path('naruto_classifier_v2')
-    model_path = Model.get_model_path(model_name="naruto_classifier_v2", version=3)
 
-    model = load_model(model_path)
+    
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    global model
+
+    # Connect to the workspace
+    ws = Workspace.from_config(path="config.json")  # Ensure you have the config.json in your directory or provide the full path.
+
+    # Download the model
+    model_obj = AzureModel(workspace=ws, name="naruto_classifier_v2", version=1)
+    model_path = model_obj.download(target_dir='.', exist_ok=True)
+
+    # Load the model architecture
+    base_model = ResNet50(weights=None, include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.6)(x)
+    predictions = Dense(3, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Load the saved weights
+    model.load_weights(model_path)
+
 
 def run(raw_data):
     data = json.loads(raw_data)['data']
     img_array = load_and_preprocess_image_from_url(data)
     class_probabilities = model.predict(img_array)
     predictions = np.argmax(class_probabilities, axis=1)
-    return predictions[0]
+    
+    class_names = ['Jiraiya', 'Sakura', 'Sasuke']
+    return class_names[predictions[0]]
 
 def load_and_preprocess_image_from_url(url):
     response = requests.get(url)
     image = Image.open(BytesIO(response.content))
+    
+    # Convert the image to RGB format
+    image = image.convert('RGB')
+    
     image = image.resize((224, 224))
     image_array = np.array(image) / 255.0
     return np.expand_dims(image_array, axis=0)
+
 
 
 # Test section
@@ -41,5 +71,5 @@ if __name__ == "__main__":
     img_array = np.array(img)
 
     # Use the processed image as input and get the prediction
-    result = run(img_array)
+    result = run(json.dumps({"data": 'https://www.pngitem.com/pimgs/m/653-6536166_sasuke-head-png-jpg-freeuse-sasuke-4th-great.png'}))
     print(result)
